a9ea311ddf87ead8b9c76202a176de34cbd19c87
sweetagar <sweetagar@gmail.com>
2025-09-22 14:58:18 +0800

    init commit with clean vue+vite+ts

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+const message = ref("Training Task App");
+</script>
+
+<template>
+  <main>
+    <h1>{{message}}</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 800px;
+  margin: 1rem auto;
+}  
+</style>
